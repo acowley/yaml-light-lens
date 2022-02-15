23980bce67d2a2a7f03e9230545f8baa39230c80
Anthony Cowley <acowley@gmail.com>
2022-02-15 08:57:16 -0500

    Bump lens upper bound

--- flake.nix
+++ flake.nix
@@ -1,42 +1,43 @@
 {
-  description = "Lens interface to yaml-light";
+  description = "Lens interface to yaml-light.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     nix-filter.url = "github:numtide/nix-filter";
-    hls.url = "github:haskell/haskell-language-server";
   };
 
-  outputs = { self, nixpkgs, flake-utils, nix-filter, ... }@inputs:
-    flake-utils.lib.eachSystem ["x86_64-linux"] (system: let
-      compiler = "ghc8104";
+  outputs = { self, nixpkgs, flake-utils, nix-filter }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      compiler = "8107";
+      # compiler = "921";
       pkgs = import nixpkgs {
         inherit system;
-        config = { allowUnfree = true; allowBroken = true; };
       };
-      hspkgs = pkgs.haskell.packages.${compiler};
-      yaml-light-lens = hspkgs.callPackage ./default.nix { nix-filter = nix-filter.lib; };
-      # ghc = hspkgs.ghc.withHoogle (ps: yaml-light-lens.passthru.getBuildInputs.haskellBuildInputs);
-      ghc = hspkgs.ghc.withHoogle (ps: [ps.base
-                                        ps.bytestring
-                                        ps.bytestring-lexing
-                                        ps.containers
-                                        ps.doctest
-                                        ps.lens
-                                        ps.yaml-light]);
+      dontCheck = pkgs.haskell.lib.dontCheck;
+      dontHaddock = pkgs.haskell.lib.dontHaddock;
+      hspkgs = (pkgs.haskell.packages."ghc${compiler}").override {
+        overrides = self: super: 
+          if compiler == "921"
+          then { 
+            # QuickCheck = dontCheck super.QuickCheck;
+            # optparse-applicative = dontCheck super.optparse-applicative;
+            # generic-deriving = dontHaddock super.generic-deriving;
+          } 
+          else { };
+      };
+      drv = hspkgs.callPackage ./default.nix {
+        nix-filter = nix-filter.lib;
+      };
+      ghc = hspkgs.ghc.withPackages (ps: 
+        drv.passthru.getBuildInputs.haskellBuildInputs
+      );
   in {
     devShell = pkgs.mkShell {
       buildInputs = [
         ghc
         hspkgs.cabal-install
-        hspkgs.haskell-language-server
       ];
-      shellHook = ''
-        export NIX_GHC=${ghc}/bin/ghc
-      '';
     };
-    packages.yaml-light-lens = yaml-light-lens;
-    defaultPackage = self.packages.${system}.yaml-light-lens;
   });
 }
